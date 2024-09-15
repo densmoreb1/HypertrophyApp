5af3cf942fa0568895b5aa74fe99738a100185a2
Brandon <densmoreb1@icloud.com>
2024-09-15 16:30:53 -0400

    progress on meso page

--- pages/Create_Meso.py
+++ pages/Create_Meso.py
@@ -1,27 +1,56 @@
 import streamlit as st
 
-st.write('Create Meso')
+st.write('# Create Meso')
 
 
-cols = st.columns(8)
+days = st.selectbox('Days per week', (1, 2, 3, 4, 5, 6, 7))
+cols = st.columns(days)
 
-with cols[0]:
-    st.write('Day 1')
+if days >= 1:
+    with cols[0]:
+        st.write('Day 1')
+        st.selectbox('Muscle Group', ('Chest', 'Back', 'Triceps',
+                     'Biceps', 'Shoulders', 'Quads', 'Calves', 'Abs', 'Hams'),
+                     key=1)
 
-with cols[1]:
-    st.write('Day 2')
+if days >= 2:
+    with cols[1]:
+        st.write('Day 2')
+        st.selectbox('Muscle Group', ('Chest', 'Back', 'Triceps',
+                     'Biceps', 'Shoulders', 'Quads', 'Calves', 'Abs', 'Hams'),
+                     key=2)
 
-with cols[2]:
-    st.write('Day 3')
+if days >= 3:
+    with cols[2]:
+        st.write('Day 3')
+        st.selectbox('Muscle Group', ('Chest', 'Back', 'Triceps',
+                     'Biceps', 'Shoulders', 'Quads', 'Calves', 'Abs', 'Hams'),
+                     key=3)
 
-with cols[3]:
-    st.write('Day 4')
+if days >= 4:
+    with cols[3]:
+        st.write('Day 4')
+        st.selectbox('Muscle Group', ('Chest', 'Back', 'Triceps',
+                     'Biceps', 'Shoulders', 'Quads', 'Calves', 'Abs', 'Hams'),
+                     key=4)
 
-with cols[4]:
-    st.write('Day 5')
+if days >= 5:
+    with cols[4]:
+        st.write('Day 5')
+        st.selectbox('Muscle Group', ('Chest', 'Back', 'Triceps',
+                     'Biceps', 'Shoulders', 'Quads', 'Calves', 'Abs', 'Hams'),
+                     key=5)
 
-with cols[5]:
-    st.write('Day 6')
+if days >= 6:
+    with cols[5]:
+        st.write('Day 6')
+        st.selectbox('Muscle Group', ('Chest', 'Back', 'Triceps',
+                     'Biceps', 'Shoulders', 'Quads', 'Calves', 'Abs', 'Hams'),
+                     key=6)
 
-with cols[6]:
-    st.write('Day 7')
+if days >= 7:
+    with cols[6]:
+        st.write('Day 7')
+        st.selectbox('Muscle Group', ('Chest', 'Back', 'Triceps',
+                     'Biceps', 'Shoulders', 'Quads', 'Calves', 'Abs', 'Hams'),
+                     key=7)
