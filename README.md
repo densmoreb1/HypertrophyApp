c4d79fa871caf58e5f8662a4da6ce8837623995e
Brandon Densmore <78512643+densmoreb1@users.noreply.github.com>
2025-02-14 13:00:22 -0500

    feature/reformatting (#3)

--- README.md
+++ README.md
@@ -6,16 +6,20 @@
 - Docker container
 - 5 or 6 week meso cycles
 - Feedback on each mucsle group
-    - Soreness from last time
-    - Pump
-    - Workload
+  - Soreness from last time
+  - Pump
+  - Workload
 - Add sets, weights, and reps each week
-    - Only adds sets if the feedback meets the criteria
+  - Only adds sets if the feedback meets the criteria
 - Able to see how many sets per muscle group in a meso cycle
 - Able to see how many sets per muscle group all time
 - Graph to see progression of exercises
 
-## TODO for Release 1.0
+## TODO
 
-- Add a complete button to do an full insert
-- Pre populate next week workout based on prev
+- Redo the current workout page to only show the one current workout
+  - Need to filter on completed
+  - Complete button to insert into mesos
+- Allow for logins
+  - https://docs.streamlit.io/develop/concepts/multipage-apps/page-and-navigation
+- Create a past workout page to view completed workout
--- pages/02_Current.py
+++ pages/02_Current.py
@@ -4,152 +4,99 @@ import streamlit as st
 st.set_page_config(page_title='Current Workout', layout='centered')
 conn = MySQLDatabase()
 
-user_sql = conn.execute_query('select distinct name from users')
-users = [u[0] for u in user_sql]
+
+# Get Users to populate current meso
+sql = conn.execute_query('select distinct name from users')
+users = [u[0] for u in sql]
 
 user_name = st.selectbox('Name', users)
-user_id = conn.execute_query('select id from users where name = %s',
-                             (user_name,))[0][0]
+user_id = conn.execute_query('select id from users where name = %s', (user_name,))[0][0]
 
-mesos_query = 'select distinct name from mesos where user_id = %s'
-mesos_sql = conn.execute_query(mesos_query, (user_id,))
-mesos = [g[0] for g in mesos_sql]
+
+# Get Meso for the selected User
+query = 'select distinct name from mesos where user_id = %s'
+sql = conn.execute_query(query, (user_id,))
+mesos = [g[0] for g in sql]
 
 meso_name = st.selectbox('Mesos', mesos)
-meso_id = conn.execute_query('select meso_id from mesos where name = %s',
-                             (meso_name,))[0][0]
-
-weeks_query = '''
-select distinct week_id from mesos where name = %s
-'''
-weeks_sql = conn.execute_query(weeks_query, (meso_name, ))
-weeks = sorted([d[0] + 1 for d in weeks_sql])
-
-week = st.selectbox('Week', weeks) - 1
-
-days_query = '''
-select distinct day_id from mesos where name = %s
-'''
-day_sql = conn.execute_query(days_query, (meso_name, ))
-days = sorted([str(d[0] + 1) for d in day_sql])
-
-day_tabs = st.tabs(days)
-
-execise_query = '''
-select distinct e.name, e.id, m.order_id
-from mesos m
-inner join exercises e
-    on m.exercise_id = e.id
-where m.day_id = %s and m.week_id = %s and m.name = %s
-order by m.order_id
-'''
-
-workout_query = '''
-select m.set_id, m.reps, m.weight, m.completed, m.order_id
-from mesos m
-inner join exercises e
-    on m.exercise_id = e.id
-where m.day_id = %s and m.week_id = %s and exercise_id = %s
-and m.name = %s
-'''
-
-update_set_query = '''
-update mesos
-set reps = %s
-, weight = %s
-, completed = 1
-, date_completed = now()
-where set_id = %s and day_id = %s and week_id = %s and exercise_id = %s
-and name = %s
-'''
-
-add_set_query = '''
-insert into fitness.mesos
-(meso_id, name, user_id, completed, set_id, reps, weight, order_id
-, exercise_id, day_id, week_id, date_created)
-values (%s, %s, %s, 0, %s, %s, %s, %s, %s, %s, %s, now())
-'''
+meso_id = conn.execute_query('select meso_id from mesos where name = %s', (meso_name,))[0][0]
 
 
-for i in range(len(day_tabs)):
-    with day_tabs[i]:
-        st.write(f'## Day {i + 1}')
+# Get the first uncompleted workout
+query = 'select min(day_id), min(week_id) from mesos where completed = 0 and meso_id = %s and user_id = %s'
+uncompleted_ids = conn.execute_query(query, (meso_id, user_id))[0]
+day_id = uncompleted_ids[0]
+week_id = uncompleted_ids[1]
 
-        exercise_sql = conn.execute_query(execise_query, (i, week, meso_name))
-        workout_dict = {}
+# Get the exercises
+query = '''
+        select distinct e.name, e.id, m.order_id
+        from mesos m
+        inner join exercises e on m.exercise_id = e.id
+        where m.day_id = %s and m.week_id = %s and m.meso_id = %s and m.user_id = %s
+        order by m.order_id
+        '''
+exercises = conn.execute_query(query, (day_id, week_id, meso_id, user_id))
 
-        for exercise in range(len(exercise_sql)):
-            st.write(f'### {exercise_sql[exercise][0]}')
-            exercise_id = exercise_sql[exercise][1]
-            workout_dict[exercise_id] = []
+for i in range(len(exercises)):
+    exercise_name = exercises[i][0]
 
-            workout_sql = conn.execute_query(workout_query,
-                                             (i, week, exercise_id, meso_name))
+    query = '''
+            select m.set_id, m.reps, m.weight, e.name, e.id, m.order_id
+            from mesos m
+            inner join exercises e on m.exercise_id = e.id
+            where m.day_id = %s and m.week_id = %s and m.meso_id = %s and m.user_id = %s and e.name = %s
+            order by m.order_id
+            '''
+    workout = conn.execute_query(query, (day_id, week_id, meso_id, user_id, exercise_name))
 
-            previous_workout = conn.execute_query(workout_query,
-                                                  (i, week - 1, exercise_id, meso_name))
+    st.write(f'### {exercise_name}')
 
-            if len(previous_workout) > 0:
-                workout_sets = previous_workout
-            else:
-                workout_sets = workout_sql
+    for i in range(len(workout)):
+        set_id = workout[i][0]
+        reps = workout[i][1]
+        weight = workout[i][2]
+        name = workout[i][3]
+        exercise_id = workout[i][4]
+        order_id = workout[i][5]
 
-            cols = st.columns(4)
-            for j in range(len(workout_sql)):
-                sets = workout_sql[j][0]
-                reps = workout_sql[j][1]
-                weight = workout_sql[j][2]
-                completed = workout_sql[j][3]
-                order_id = workout_sql[j][4]
+        cols = st.columns(4)
+        with cols[0]:
+            st.write(f'Set: {set_id + 1}')
+        with cols[1]:
+            weight = st.number_input('Weight',
+                                     label_visibility='collapsed',
+                                     value=None,
+                                     key=f'weight{exercise_name, set_id}',
+                                     step=.5)
+        with cols[2]:
+            reps = st.number_input('Reps',
+                                   label_visibility='collapsed',
+                                   value=None,
+                                   key=f'reps{exercise_name, set_id}',
+                                   step=1)
+        with cols[3]:
+            completed = st.checkbox('Complete', key=f'completed{exercise_name, set_id}')
 
-                with cols[0]:
-                    st.write(f'Set: {sets + 1}')
-                with cols[1]:
-                    if completed == 0:
-                        weight = st.number_input('Weight',
-                                                 label_visibility='collapsed',
-                                                 value=None,
-                                                 placeholder=workout_sets[-1][2],
-                                                 key=f'weight{exercise, i, j}',
-                                                 step=.5)
-                    else:
-                        st.write(f'Weight: {weight}')
-                with cols[2]:
-                    if completed == 0:
-                        reps = st.number_input('Reps',
-                                               label_visibility='collapsed',
-                                               value=None,
-                                               placeholder=workout_sets[-1][1],
-                                               key=f'reps{exercise, i, j}',
-                                               step=1)
-                    else:
-                        st.write(f'Reps: {reps}')
-                with cols[3]:
-                    if completed == 0:
-                        c = st.checkbox('Complete',
-                                        key=f'completed{exercise, i, j}')
-                        if c:
-                            s = {'weight': weight, 'reps': reps}
-                            workout_dict[exercise_id].append(s)
-                    else:
-                        st.write('Completed')
+        if completed:
+            query = '''
+                    update mesos
+                    set reps = %s, weight = %s, completed = 1, date_completed = now()
+                    where set_id = %s and day_id = %s and week_id = %s and exercise_id = %s and name = %s
+                    '''
+            conn.execute_query(query, (reps, weight, set_id, day_id, week_id, exercise_id, meso_name))
 
-            add_set = st.button('Add set', key=f'add{exercise, i}')
-            if add_set:
-                conn.execute_query(add_set_query,
-                                   (meso_id, meso_name, user_id,
-                                    sets + 1, 0, 0,
-                                    order_id, exercise_id, i, week)
-                                   )
-                st.rerun()
+    add_set = st.button('Add set', key=f'add{exercise_name, set_id}')
+    if add_set:
+        query = '''
+                insert into mesos
+                (meso_id, name, user_id, completed, set_id, reps, weight, order_id, exercise_id, day_id, week_id, date_created) values
+                (%s,        %s,      %s,         0,     %s,    %s,    %s,       %s,          %s,     %s,      %s, now())
+                '''
+        conn.execute_query(query, (meso_id, meso_name, user_id, set_id + 1, reps, weight, order_id, exercise_id, day_id, week_id))
+        st.toast('Inserted')
+        st.rerun()
 
-        st.write('###')
-        complete_workout = st.button('Complete Workout', key=f'complete{exercise, i}')
-
-        if complete_workout:
-            for exercise_id, sets in workout_dict.items():
-                for s in range(len(sets)):
-                    conn.execute_query(
-                        update_set_query,
-                        (sets[s]['reps'], sets[s]['weight'], s, i, week, exercise_id, meso_name))
-                    st.rerun()
+# Complete workout - navigate to previous workout page
+st.write('###')
+all_done = st.button('Complete Workout')
