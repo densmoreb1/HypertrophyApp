fad92370bea7c516fdba578529ccc6699d4ebd56
Brandon Densmore <hens.tippet.1o@icloud.com>
2026-03-15 08:46:01 -0400

    backups

--- backups.py
+++ backups.py
@@ -1,13 +1,23 @@
 from dotenv import load_dotenv
+import argparse
 import datetime
 import os
 import subprocess
 
-load_dotenv("./.env")
+parser = argparse.ArgumentParser(
+    description="Creates a backup file with a date key in the file name"
+)
+
+parser.add_argument("--env", help="Env full file path")
+parser.add_argument("--backup", help="Backup directory path")
+
+args = parser.parse_args()
+
+load_dotenv(args.env)
 
 date_str = datetime.datetime.now().strftime("%Y%m%d")
 
-backup_file = f"./backup{date_str}.sql"
+backup_file = f"{args.backup}/backup{date_str}.sql"
 mysql_password = os.getenv("DB_PASSWORD")
 
 cmd = [
